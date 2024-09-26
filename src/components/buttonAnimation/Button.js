ae2bd85bb6c034fe7655a8b97c6791207a19fc3d
ben-1013 <benjamintan1013@gmail.com>
2024-09-25 17:18:49 -0700

    sidebar

--- src/components/buttonAnimation/Button.js
+++ src/components/buttonAnimation/Button.js
@@ -1,14 +1,35 @@
 import Image from "next/image";
-import React,{useState} from "react";
+import React,{useState,useEffect} from "react";
 import {motion} from "framer-motion";
 
 const ButtonAnimation = () => {
     const [isOpen,setIsOpen] = useState(false);
+    const [isAirdropSidebar,setAirdropSidebar] = useState(false);
 
     const airdropAction = () => {
         // useAirdrop();
-        setIsOpen(!isOpen); // Toggle sidebar visibility
+        if(!isAirdropSidebar) {
+            setAirdropSidebar(!isAirdropSidebar); // Toggle sidebar visibility
+        }
     };
+
+    useEffect(() => {
+        const handleOutsideClick = (event) => {
+            if(!event.target.closest(".sidebar") && isAirdropSidebar === true && isOpen == false) {
+                setIsOpen(true);
+            }
+            if(!event.target.closest(".sidebar") && isOpen == true) {
+                setIsOpen(false);
+                setAirdropSidebar(false);
+            }
+        };
+        document.addEventListener("click",handleOutsideClick);
+
+        return () => {
+            document.removeEventListener("click",handleOutsideClick);
+        };
+    },);
+
 
     return (
         <>
@@ -29,12 +50,11 @@ const ButtonAnimation = () => {
                 <Image src="/monkey/airdrop.png" width={60} height={60} alt="airdrop" />
             </button>
 
-            {/* Left Sidebar */}
-            <motion.div
-                className={`fixed left-0 top-0 h-full w-1/3 bg-gradient-to-r from-yellow-400 to-orange-500 p-5 ${isOpen ? "block" : "hidden"
+            <motion.div id="sidebar"
+                className={`z-50 fixed left-0 top-0 h-full w-1/3 bg-gradient-to-r from-yellow-400 to-orange-500 p-5 ${isAirdropSidebar ? "block" : "hidden"
                     }`}
                 initial={{x: -300}}
-                animate={{x: isOpen ? 0 : -300}}
+                animate={{x: isAirdropSidebar ? 0 : -300}}
                 transition={{type: "spring",stiffness: 300}}
             >
                 <h2 className="text-2xl font-bold text-pink-600">Airdrop Criteria</h2>
@@ -43,11 +63,11 @@ const ButtonAnimation = () => {
                     <li>Criteria 2: ...</li>
                     <li>Criteria 3: ...</li>
                 </ul>
-                <h2 className="text-2xl font-bold mt-4 text-teal-600">Method</h2>
+                <h2 className="mt-4 text-2xl font-bold text-teal-600">Method</h2>
                 <p className="text-lg text-green-600">Explain your airdrop method here.</p>
                 <button
                     onClick={() => setIsOpen(false)}
-                    className="mt-4 bg-red-500 hover:bg-red-700 text-white py-2 px-4 rounded"
+                    className="px-4 py-2 mt-4 text-white bg-red-500 rounded hover:bg-red-700"
                 >
                     Close
                 </button>
