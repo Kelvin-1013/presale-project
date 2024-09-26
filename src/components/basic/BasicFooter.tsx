1e2b29b053a75b67188937b357901383fd0554b5
ben-1013 <benjamintan1013@gmail.com>
2024-09-25 23:44:30 -0700

    restore

--- src/components/basic/BasicFooter.tsx
+++ src/components/basic/BasicFooter.tsx
@@ -1,32 +1,34 @@
 import React from 'react';
 import { useRouter } from 'next/router';
-import Image from 'next/image';
 import { FaTwitterSquare, FaGithub, FaDiscord, FaInstagram, FaTelegram } from "react-icons/fa";
-import XIcon from "./X_icon.png"
+import { SiLinktree } from "react-icons/si";
 
-const TWITTER_LINK = 'https://twitter.com/@TMONK777';
-const GITHUB_LINK = 'https://github.com/TowerTransition';
-const TREE_LINK = 'https://linktr.ee/';
-const INSTAGRAM_LINK = 'https://www.instagram.com/tmonk777/';
-const DISCORD_LINK = 'https://discord.gg/2aNj737F';
-const TELEGRAM_LINK = 'https://t.me/+gcK4YOe5sXZjZWY5';
+const TWITTER_LINK = 'https://twitter.com/@TMONK777'
+const GITHUB_LINK = 'https://github.com/TowerTransition'
+const TREE_LINK = 'https://linktr.ee/'
+const INSTAGRAM_LINK = 'https://www.instagram.com/tmonk777/'
+const DISCORD_LINK = 'https://discord.gg/2aNj737F'
+const TELEGRAM_LINK = 'https://t.me/+gcK4YOe5sXZjZWY5'
 
 const BasicFooter = () => {
-  const router = useRouter();
+  const router = useRouter()
 
   return (
-    <div className='flex flex-col justify-center py-16 text-center align-middle bg-cA text-cC'>
-      <p className='font-light'>Copyright © 2024 - All right reserved</p>
+    <div className='flex flex-col justify-center py-16 text-center align-middle bg-cA text-cC '>
+      <img src={'/images/MGLogo.png'} alt="" className='h-[50px] w-[50px] mx-auto mb-4 cursor-pointer' onClick={() => window.open(TREE_LINK, "_blank")} />
+      {/* <h2>MoonGhost Labs Ltd.</h2> */}
+      <p className='font-light '>Copyright © 2024 - All right reserved</p>
       <div className='flex flex-row justify-center align-middle'>
-        <Image src={XIcon} alt="" className='mx-6 mt-4 h-[50px] w-[50px] m-3 text-gray-800 transition-all duration-300 cursor-pointer hover:scale-75' onClick={() => window.open(TWITTER_LINK, "_blank")} />
-        <FaGithub size={50} className='mx-6 mt-4 text-gray-800 transition-all duration-300 cursor-pointer hover:text-gray-600 hover:scale-90' onClick={() => window.open(GITHUB_LINK, "_blank")} />
-        <FaDiscord size={50} className='mx-6 mt-4 text-indigo-600 transition-all duration-300 cursor-pointer hover:text-indigo-800 hover:scale-90' onClick={() => window.open(DISCORD_LINK, "_blank")} />
-        <FaInstagram size={50} className='mx-6 mt-4 text-pink-500 transition-all duration-300 cursor-pointer hover:text-pink-700 hover:scale-90' onClick={() => window.open(INSTAGRAM_LINK, "_blank")} />
-        <FaTelegram size={50} className='mx-6 mt-4 text-blue-400 transition-all duration-300 cursor-pointer hover:text-blue-600 hover:scale-90' onClick={() => window.open(TELEGRAM_LINK, "_blank")} />
-        {/* <SiTwitter size={50} className='mx-6 mt-4 text-green-500 transition-all duration-300 cursor-pointer hover:text-green-700' onClick={() => window.open(TREE_LINK, "_blank")} /> */}
+        <FaTwitterSquare size={50} className='mx-6 mt-4 transition-all duration-300 cursor-pointer text-cC hover:text-cB' onClick={() => window.open(TWITTER_LINK, "_blank")} />
+        <FaGithub size={50} className='mx-6 mt-4 duration-300 cursor-pointer text-cC hover:text-cB' onClick={() => window.open(GITHUB_LINK, "_blank")} />
+        <FaDiscord size={50} className='mx-6 mt-4 duration-300 cursor-pointer text-cC hover:text-cB' onClick={() => window.open(DISCORD_LINK, "_blank")} />
+        <FaInstagram size={50} className='mx-6 mt-4 duration-300 cursor-pointer text-cC hover:text-cB' onClick={() => window.open(INSTAGRAM_LINK, "_blank")} />
+        <FaTelegram size={50} className='mx-6 mt-4 duration-300 cursor-pointer text-cC hover:text-cB' onClick={() => window.open(TELEGRAM_LINK, "_blank")} />
+        <SiLinktree size={50} className='mx-6 mt-4 duration-300 cursor-pointer text-cC hover:text-cB' onClick={() => window.open(TREE_LINK, "_blank")} />
       </div>
+
     </div>
-  );
+  )
 }
 
-export default BasicFooter;
+export default BasicFooter
