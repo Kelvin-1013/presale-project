b238b7440f76da265e93ec8a19162ea198ef204e
Kelvin-1013 <tonybanana1013@gmail.com>
2024-10-24 16:01:19 -0700

    Update presalePanel.tsx

--- app/src/components/landing/presalePanel.tsx
+++ app/src/components/landing/presalePanel.tsx
@@ -58,7 +58,7 @@ const PresalePanel = () => {
 
             if (distance < 0) {
                 setTimeRemaining({ days: 0, hours: 0, minutes: 0, seconds: 0 });
-                setProgress(2);
+                setProgress(0);
                 return;
             }
 
@@ -87,28 +87,28 @@ const PresalePanel = () => {
                 <meta name="description" content="Join the most promising meme coin of today!" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <div className="font-bold animated-heading bg-[#2cc433]"><h1 className='text-5xl'>Presale</h1></div>
-            <p className="text-xl mb-2 text-green-800 outline-cA font-bold font-roboto">Buy before the price goes up!</p>
+            <div className="font-bold animated-heading bg-[#2cc433]"><h1 className='text-5xl'>Presale  Soon</h1></div>
+            <p className="mb-2 text-xl font-bold text-green-800 outline-cA font-roboto">Buy before the price goes up!</p>
             <div className="flex flex-col rounded-xl items-center bg-[#f7f7f7c4] shadow-md">
-                <div className="flex space-x-2 px-4">
-                    <div className="text-green-800 px-4 rounded text-center">
+                <div className="flex px-4 space-x-2">
+                    <div className="px-4 text-center text-green-800 rounded">
                         <span className="text-2xl font-bold">{timeRemaining.days}</span>
                         <div className="text-sm">days</div>
                     </div>
-                    <div className="text-green-800 px-4 rounded text-center">
+                    <div className="px-4 text-center text-green-800 rounded">
                         <span className="text-2xl font-bold">{timeRemaining.hours}</span>
                         <div className="text-sm">hours</div>
                     </div>
-                    <div className="text-green-800 px-4 rounded text-center">
+                    <div className="px-4 text-center text-green-800 rounded">
                         <span className="text-2xl font-bold">{timeRemaining.minutes}</span>
                         <div className="text-sm">min</div>
                     </div>
-                    <div className="text-green-800 px-4 rounded text-center">
+                    <div className="px-4 text-center text-green-800 rounded">
                         <span className="text-2xl font-bold">{timeRemaining.seconds}</span>
                         <div className="text-sm">sec</div>
                     </div>
                 </div>
-                <p className="text-center text-purple-700 text-sm ">{progress.toFixed(2)}% completed</p>
+                <p className="text-sm text-center text-purple-700 ">{progress.toFixed(2)}% completed</p>
                 <div className="w-full bg-gray-300 rounded-b-md">
                     <div
                         className={`h-4 rounded-bl-lg rounded-r-lg ${progress < 100 ? 'bg-orange-500' : 'bg-green-500'} progress-bar`}
@@ -116,87 +116,87 @@ const PresalePanel = () => {
                     ></div>
                 </div>
             </div>
-            <div className="flex flex-row space-x-4 mt-2">
-                <div className=" px-1 py-1 rounded flex flex-col justify-between w-full animate-background-fade">
-                    <div className="flex flex-col justify-between items-center">
-                        <div className="text-white font-bold whitespace-nowrap">ROUND - 1</div>
-                        <div className="text-white text-xs leading-tight whitespace-nowrap ">LIVE</div> {/* Added background animation class */}
+            <div className="flex flex-row mt-2 space-x-4">
+                <div className="flex flex-col justify-between w-full px-1 py-1 rounded animate-background-fade">
+                    <div className="flex flex-col items-center justify-between">
+                        <div className="font-bold text-white whitespace-nowrap">ROUND - 1</div>
+                        <div className="text-xs leading-tight text-white whitespace-nowrap ">SOON</div> {/* Added background animation class */}
                     </div>
-                    <div className="text-yellow-300 leading-tight whitespace-nowrap">150,000,000</div>
-                    <div className="text-white leading-tight whitespace-nowrap">Price: $0.0025</div>
+                    <div className="leading-tight text-yellow-300 whitespace-nowrap">150,000,000</div>
+                    <div className="leading-tight text-white whitespace-nowrap">Price: $0.0025</div>
                 </div>
 
-                <div className="bg-blue-600 px-1 py-1 rounded flex flex-col justify-between w-full">
-                    <div className="flex flex-col justify-between items-center">
-                        <div className="text-white font-bold whitespace-nowrap">ROUND - 2</div>
-                        <div className="text-blue-200 text-xs leading-tight whitespace-nowrap">COMING SOON</div>
+                <div className="flex flex-col justify-between w-full px-1 py-1 bg-blue-600 rounded">
+                    <div className="flex flex-col items-center justify-between">
+                        <div className="font-bold text-white whitespace-nowrap">ROUND - 2</div>
+                        <div className="text-xs leading-tight text-blue-200 whitespace-nowrap">COMING SOON</div>
                     </div>
-                    <div className="text-yellow-300 leading-tight whitespace-nowrap">200,000,000</div>
-                    <div className="text-white leading-tight whitespace-nowrap">Price: $0.004</div>
+                    <div className="leading-tight text-yellow-300 whitespace-nowrap">200,000,000</div>
+                    <div className="leading-tight text-white whitespace-nowrap">Price: $0.004</div>
                 </div>
 
-                <div className="bg-blue-600 px-1 py-1 rounded flex flex-col justify-between w-full">
-                    <div className="flex flex-col justify-between items-center">
-                        <div className="text-white font-bold whitespace-nowrap">ROUND - 3</div>
-                        <div className="text-blue-200 text-xs leading-tight whitespace-nowrap">COMING SOON</div>
+                <div className="flex flex-col justify-between w-full px-1 py-1 bg-blue-600 rounded">
+                    <div className="flex flex-col items-center justify-between">
+                        <div className="font-bold text-white whitespace-nowrap">ROUND - 3</div>
+                        <div className="text-xs leading-tight text-blue-200 whitespace-nowrap">COMING SOON</div>
                     </div>
-                    <div className="text-yellow-300 leading-tight whitespace-nowrap">50,000,000</div>
-                    <div className="text-white leading-tight whitespace-nowrap">Price: $0.008</div>
+                    <div className="leading-tight text-yellow-300 whitespace-nowrap">50,000,000</div>
+                    <div className="leading-tight text-white whitespace-nowrap">Price: $0.008</div>
                 </div>
             </div>
-            <div className="mt-4 flex flex-col items-center w-full">
-                <label htmlFor="amount" className="block text-green-900 font-bold">
+            <div className="flex flex-col items-center w-full mt-4">
+                <label htmlFor="amount" className="block font-bold text-green-900">
                     Enter the amount in SOLANA, min. 0.25 SOL:
                 </label>
-                <label htmlFor="amount" className="block text-purple-800 font-bold">
+                <label htmlFor="amount" className="block font-bold text-purple-800">
                     Now, your balance is
-                    <span className="text-red-500 mx-1">
+                    <span className="mx-1 text-red-500">
                         {walletBalance}
                     </span>
                     SOL
                 </label>
 
-                <div className="flex justify-end mt-1 w-full">
+                <div className="flex justify-end w-full mt-1">
                     <input
                         type="number"
                         id="amount"
-                        className="px-2 w-full py-1 rounded-l-lg border-2 border-yellow-400 bg-white text-green-900 focus:outline-none focus:ring-2 focus:ring-yellow-400"
+                        className="w-full px-2 py-1 text-green-900 bg-white border-2 border-yellow-400 rounded-l-lg focus:outline-none focus:ring-2 focus:ring-yellow-400"
                         placeholder="0"
                         min="0.25"
                         value={solAmount}
                         onChange={(e) => setSolAmount(parseFloat(e.target.value))}
                     />
-                    <button className="bg-yellow-400 text-white px-7  py- rounded-r-lg shadow-md hover:bg-yellow-500 transition duration-300">
+                    <button className="text-white transition duration-300 bg-yellow-400 rounded-r-lg shadow-md px-7 py- hover:bg-yellow-500">
                         SOL
                     </button>
                 </div>
-                <div className="flex justify-end mt-2 w-full">
+                <div className="flex justify-end w-full mt-2">
                     <input
                         type="number"
                         id="amount"
-                        className="px-2 w-full py-1 rounded-l-lg border-2 border-yellow-400 bg-white text-green-900 focus:outline-none focus:ring-2 focus:ring-yellow-400"
+                        className="w-full px-2 py-1 text-green-900 bg-white border-2 border-yellow-400 rounded-l-lg focus:outline-none focus:ring-2 focus:ring-yellow-400"
                         placeholder="0"
                         value={tokenAmount}
                         onChange={setManualTokenAmount}
                     />
-                    <button className="bg-yellow-400 text-white px-4 py- rounded-r-lg shadow-md hover:bg-yellow-500 transition duration-300">
+                    <button className="px-4 text-white transition duration-300 bg-yellow-400 rounded-r-lg shadow-md py- hover:bg-yellow-500">
                         TMONK
                     </button>
                 </div>
             </div>
 
-            <div className="mt-2 text-green-900 w-full">
+            <div className="w-full mt-2 text-green-900">
                 <label htmlFor="email" className="block ">Your E-mail address (gmail.com):</label>
-                <input type="email" id="email" className="p-1 rounded text-green-800 w-full" placeholder="your email" />
+                <input type="email" id="email" className="w-full p-1 text-green-800 rounded" placeholder="your email" />
             </div>
 
-            <div className="mt-2 w-full">
-                <label htmlFor="solanaAddress" className="block w-full min-w-max text-green-800">Your SOLANA address:</label>
+            <div className="w-full mt-2">
+                <label htmlFor="solanaAddress" className="block w-full text-green-800 min-w-max">Your SOLANA address:</label>
                 <input type="text" id="solanaAddress"
-                    value={walletAddress} onChange={changeWalletAddress} className="p-1 rounded text-green-800 w-full" placeholder="your SOLANA address" />
+                    value={walletAddress} onChange={changeWalletAddress} className="w-full p-1 text-green-800 rounded" placeholder="your SOLANA address" />
             </div>
 
-            <button className="mt-2 bg-yellow-500 text-green-900 px-6 py-2 rounded-xl text-lg font-bold">BUY $TMONK</button>
+            <button className="px-6 py-2 mt-2 text-lg font-bold text-green-900 bg-yellow-500 rounded-xl">BUY $TMONK</button>
         </div>
 
     );
